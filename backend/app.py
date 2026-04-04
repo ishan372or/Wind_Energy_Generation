29620e5ffc189fd3371660b917f1cc89cd048ba0
ishan372or <ishan372or@gmail.com>
2026-04-04 09:35:54 +0530

    removed imports from app.py

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,6 @@
 import sys
 import os
 from flask import Flask, jsonify, request
-from pipelines.training_pipeline import training_pipeline
-from pipelines.monthly_update_pipeline import monthly_update
 import logging
 import mlflow
 import psycopg2
@@ -24,33 +22,6 @@ STATES = [
     "Minnesota", "North Dakota", "Oklahoma", "Texas", "Washington"
 ]
 MODELS = ["XGBoost", "LightGBM"]
- 
-WEATHER_PATHS = {
-    "California":   "raw/weather/California weather.csv",
-    "Colorado":     "raw/weather/colorado weather.csv",
-    "Illinois":     "raw/weather/Illinois weather.csv",
-    "Iowa":         "raw/weather/Iowa weather.csv",
-    "Kansas":       "raw/weather/kansas weather.csv",
-    "Minnesota":    "raw/weather/Minnesota weather.csv",
-    "North Dakota": "raw/weather/North Dakota weather.csv",
-    "Oklahoma":     "raw/weather/Oklahoma weather.csv",
-    "Texas":        "raw/weather/Texas weather.csv",
-    "Washington":   "raw/weather/Washington weather.csv",
-}
- 
-ENERGY_PATH = "raw/Net_Energy_Generation/Top 10 States net Generation.csv"
-
-def monthly_update():
-    try:
-        logging.info("Monthly update triggered")
-        dagshub.init(
-            repo_owner=os.getenv("DAGSHUB_REPO_OWNER"),
-            repo_name=os.getenv("DAGSHUB_REPO_NAME"),
-            mlflow=True
-        )
-        monthly_update()
-    except Exception as e:
-        logging.error(f"Monthly update failed: {e}")
         
 @app.route("/models", methods=["GET"])
 def get_models():
@@ -99,16 +70,5 @@ def get_forecast():
         logging.error(f"Error fetching forecast for {state}: {e}")
         return jsonify({"error": str(e)}), 500
     
-scheduler = BackgroundScheduler()
-scheduler.add_job(
-    func=monthly_update,
-    trigger="cron",
-    month=1,
-    day=2,
-    hour=0,
-    minute=0
-)
-scheduler.start()
-
 if __name__=="__main__":
     app.run(debug=True,port=5000)
