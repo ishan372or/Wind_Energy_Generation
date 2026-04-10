0230f16760f12d119f944856f074b2a8ac681405
ishan372or <ishan372or@gmail.com>
2026-04-10 23:45:22 +0530

    adding logic

--- src/store_predictions.py
+++ src/store_predictions.py
@@ -7,50 +7,53 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
- 
-@step
+
+@step(enable_cache=False)
 def predict_and_store(
     xgb_model,
     lgb_model,
     cat_model,
     df: pd.DataFrame
 ):
-    preprocessor= joblib.load("preprocessor.pkl")
-    DB_URL= os.getenv("DATABASE_URL")
+    preprocessor = joblib.load("preprocessor.pkl")
+
+    DB_URL = os.getenv("DATABASE_URL")
+    if DB_URL is None:
+        raise ValueError("DATABASE_URL not found in environment variables")
+
     conn = psycopg2.connect(DB_URL)
-    cursor= conn.cursor()
-    
+    cursor = conn.cursor()
+
     test_df = df[df["Month_Year"] >= "2023-01-01"]
- 
-    try:
-        cursor.execute(
-            "SELECT DISTINCT month, state, model_name FROM predictions WHERE predicted IS NOT NULL"
-        )
-        rows= cursor.fetchall()
-        stored_pairs= set(rows)  
-    except:
-        stored_pairs = set()
- 
-    for model, model_name in [(xgb_model, "XGBoost"), (lgb_model, "LightGBM"), (cat_model, "CatBoost")]:
+
+    for model, model_name in [
+        (xgb_model, "XGBoost"),
+        (lgb_model, "LightGBM"),
+        (cat_model, "CatBoost")
+    ]:
         for _, row in test_df.iterrows():
-            month = row["Month_Year"].strftime("%Y-%m")
-            state = row["Region"]
- 
-            if (month, state, model_name) in stored_pairs:
-                continue
- 
-            row_df = pd.DataFrame([row])
-            X, _ = preprocessor.transform(row_df)
-            pred = float(model.predict(X)[0])
-            actual = float(row["Net_Generation_MWh"])  
-            cursor.execute("""
-                UPDATE predictions
-                SET predicted = %s
-                WHERE state = %s AND month = %s AND model_name = %s AND predicted IS NULL
-            """, (pred, state, month, model_name))
-            conn.commit()
- 
-            logging.info(f"Stored {model_name} prediction for {state} {month}")
-    
+            try:
+                month = row["Month_Year"].strftime("%Y-%m")
+                state = row["Region"]
+
+                row_df = pd.DataFrame([row])
+                X, _ = preprocessor.transform(row_df)
+
+                pred = float(model.predict(X)[0])
+                
+                cursor.execute("""
+                    UPDATE predictions
+                    SET predicted = %s
+                    WHERE state = %s 
+                      AND month = %s 
+                      AND model_name = %s 
+                      AND predicted IS NULL
+                """, (pred, state, month, model_name))
+
+                logging.info(f"Stored {model_name} prediction for {state} {month}")
+
+            except Exception as e:
+                logging.error(f"Error processing {model_name} for {state}: {e}")
+    conn.commit()
     cursor.close()
     conn.close()
